b65be1d2dc4017a77aedbe0f38c1356f549e1ab5
CHAI RYAN <2205145B@student.tp.edu.sg>
2024-05-03 11:08:18 +0800

    Added identity server

--- majorproject/Server/Models/ApplicationUser.cs
+++ majorproject/Server/Models/ApplicationUser.cs
@@ -4,5 +4,9 @@ namespace majorproject.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? position { get; set; }
+
     }
 }
