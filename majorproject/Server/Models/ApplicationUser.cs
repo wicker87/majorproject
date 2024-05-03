7e0d29863e3a69d31aadc994cd79e3f01aee8cd3
jjpeh <jjpeh@LAPTOP-I971RUIG>
2024-05-03 15:40:43 +0800

    Frontend design

--- majorproject/Server/Models/ApplicationUser.cs
+++ majorproject/Server/Models/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace majorproject.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePic { get; set; }
     }
 }
